a78fd051de3237243dbc1e4276629624f10f8ffe
sampada109 <sampadakaushik2518@gmail.com>
2023-10-19 14:23:20 +0530

    Second Commit of Day2

---
+++ Day1/output2.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>           //bitsstdc++.h is use to include all the library in c++ so that we do not have to include any specifically again and again
+using namespace std;              //we are declaring to use namespace std so that we have to not use it before cout/cin again and again
+
+int main(){
+    cout<<"Hello World once again!";
+    cout<<"This is First Line"<<endl<<"This is Second Line";
+    //endl is use to print cout in next line
+}
