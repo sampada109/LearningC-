3bef6aa94503051ea13db619909665d56f6ad93b
sampada109 <sampadakaushik2518@gmail.com>
2023-10-19 14:13:18 +0530

    First Commit of Day1

--- Day1/output.cpp
+++ Day1/output.cpp
@@ -1,5 +1,5 @@
-#include<iostream>
+#include<iostream>                      //header which we have to include in every code
 
 int main(){
-    std::cout<<"Hello World";
+    std::cout<<"Hello World";          //by using std we are able to print o/p and take i/p        
 }
